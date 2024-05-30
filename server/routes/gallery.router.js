91d2c79961d1a3f9c285fdcd9a2b79d10cad1b30
Joshua Bass <joshuabass77@gmail.com>
2024-05-30 16:56:56 -0500

    got get router up

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
@@ -8,7 +9,19 @@ router.put('/like/:id', (req, res) => {
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const queryText = `
+  SELECT  * FROM "gallery"
+  `
+
+  pool.query(queryText)
+  .then((response)=>{
+    console.log("response from get router", response.rows)
+    res.send(response.rows)
+
+  })
+  . catch((error)=>{
+    console.error("error in get router", error )
+  })
 });
 
 module.exports = router;
